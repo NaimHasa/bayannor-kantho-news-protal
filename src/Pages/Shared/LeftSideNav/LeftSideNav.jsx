a17dccef0e65ff763e28289eb605ab87f82f981d
Naim Hasan <naimarafat7@gmail.com>
2023-07-02 01:20:44 -0700

    left-side-nav

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,10 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
     const [categrouies, setCategrouies] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/news-categrouies')
+            .then(res => res.json())
+            .then(data => setCategrouies(data))
+    }, [])
     return (
         <div>
             <h4>All Categrouies</h4>
+            {
+                categrouies.map(categroy => <p key={categroy.id}>
+                    <Link to={`/category/${categroy.id}`} >{categroy.name}</Link>
+                </p>)
+            }
         </div>
     );
 };
